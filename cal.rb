b2dad6c7cca98e6180788bfd566be2206d674df2
SathishRamadoss <40665079+SathishRamadoss@users.noreply.github.com>
2018-08-29 14:43:03 +0530

    Create cal.rb

---
+++ cal.rb
@@ -0,0 +1,23 @@
+def add(a, b)
+  puts "addition answer --- #{a+b}"
+end
+
+def subtract(a, b)
+  puts "subtraction  answer --- #{a-b}"
+ 
+end
+
+def multiply(a, b)
+  puts "multiplication  answer --- #{a*b}"
+  
+end
+
+def divide(a, b)
+  puts " division  answer ---#{a/b}"
+  
+end
+
+add(10,10)
+subtract(50,4)
+multiply(40,5)
+divide(12,2)
